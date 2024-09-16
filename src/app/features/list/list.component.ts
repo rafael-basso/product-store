bdd2546dcd9443b0197463930c8f35202272c666
Rafael <rafael_basso81@hotmail.com>
2024-09-16 10:11:48 -0300

    Ajustar link - criar produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -3,11 +3,13 @@ import { Component, inject } from '@angular/core';
 import { ProductsService} from '../../shared/services/products.service'
 import { Product } from '../../shared/interfaces/product.interface';
 import { CardComponent } from './components/card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButton],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
