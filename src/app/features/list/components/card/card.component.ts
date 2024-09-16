b9c9034e4a791f469767372dad6b622e14e97daf
Rafael <rafael_basso81@hotmail.com>
2024-09-16 09:41:02 -0300

    Criar card component para renderizar no html / adicionar propriedade do produto

---
+++ src/app/features/list/components/card/card.component.ts
@@ -0,0 +1,19 @@
+import { Component, computed, input } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import { Product } from '../../../../shared/interfaces/product.interface';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [MatButtonModule, MatCardModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+  product = input.required<Product>();
+
+  productId = computed(() =>  this.product().id);
+  productTitle = computed(() =>  this.product().title);
+  productDescription = computed(() =>  this.product().description);
+}
--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,13 +2,12 @@
 import { Component, inject } from '@angular/core';
 import { ProductsService} from '../../shared/services/products.service'
 import { Product } from '../../shared/interfaces/product.interface';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import { CardComponent } from './components/card/card.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
