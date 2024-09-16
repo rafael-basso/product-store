c4bb537a8f3eb0411f60186ab71d55e4c203ac6e
Rafael <rafael_basso81@hotmail.com>
2024-09-16 09:03:26 -0300

    Importar componentes - card e button

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -2,11 +2,13 @@
 import { Component, inject } from '@angular/core';
 import { ProductsService} from '../../shared/services/products.service'
 import { Product } from '../../shared/interfaces/product.interface';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
