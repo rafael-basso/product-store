0dc4687dfc6f4f6b78bf839dc017cc0a36edd5b3
rafael-basso <rafael_basso81@hotmail.com>
2024-09-22 14:08:12 -0300

    Estilizar pagina / inserir rota voltar para home no editar produto

--- src/app/shared/components/main/main.component.ts
+++ src/app/shared/components/main/main.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
